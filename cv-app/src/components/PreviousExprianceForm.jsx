0017604db1cf9a76bc134750f11341d5063ce886
MarwanZahran25 <marwanzahran07@gmail.com>
2024-07-23 11:50:54 +0300

    fixed Typo

--- cv-app/src/components/PreviousExprianceForm.jsx
+++ cv-app/src/components/PreviousExprianceForm.jsx
@@ -2,7 +2,7 @@ import TextArea from "./TextArea";
 export default function PreviousExprianceForm({ onChange, state }) {
   return (
     <div className="px-16 font-bold flex flex-col gap-4 max-w-[700px] ">
-      <h1 className="text-2xl">Enter Any Previous Expriance You Have</h1>
+      <h1 className="text-2xl">Enter Any Previous Experience You Have</h1>
       <TextArea onChange={onChange} state={state} />
     </div>
   );
